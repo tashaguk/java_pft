9dee2da0fa28347c30414aac2c07ad73f14a46f5
tashaguk <154615573+tashaguk@users.noreply.github.com>
2024-01-17 19:29:38 +0400

    Добавила много текста и функций

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,10 +1,22 @@
 public class MyFirstProgram {
     public static void main(String[] args) {
-        String somebody = "world";
+       hello("world");
+       hello("user");
+       hello("Алексей");
+       double l = 5;
+        System.out.println("Площадь квадрата со стороной " + l + "=" + area(l) );
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    }
+    public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
+    }
+    public static double area(double len) {
+        return len*len;
+    }
 
-        double l = 8.0;
-        double s = l * 1;
-        System.out.println("Площадь квадрата со стороной " + 1 + " = " + s);
+    public static double area (double a, double b) {
+        return a*b;
     }
 }
