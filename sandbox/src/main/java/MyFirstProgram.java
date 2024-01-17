a2124531fbbab9c25f577f91a04fc03c3bfb6ff9
tashaguk <154615573+tashaguk@users.noreply.github.com>
2024-01-17 18:36:20 +0400

    1

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,10 +1,10 @@
+package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
     public static void main(String[] args) {
-        String somebody = "world";
-        System.out.println("Hello, " + somebody + "!");
+        hello();
+    }
 
-        double l = 8.0;
-        double s = l * 1;
-        System.out.println("Площадь квадрата со стороной " + 1 + " = " + s);
+    public.static.void hello() {
+        System.out.println("Hello, world");
     }
 }
